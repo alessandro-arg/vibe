f357aa9f186b7af52aeb5b92b50157e44d8147b6
Alessandro Argenziano <1603alessandro@gmail.com>
2025-12-27 15:04:59 +0100

    update: everything is connected and we get a working nextjs app from docker

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -2,6 +2,7 @@ import { Sandbox } from "@e2b/code-interpreter";
 
 import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -22,8 +23,13 @@ export const helloWorld = inngest.createFunction(
     const { output } = await codeAgent.run(
       `Write the following snippet: : ${event.data.value}`
     );
-    console.log(output);
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   }
 );
