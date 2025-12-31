f9ff607db768af786fcf16b1688237b06fdce76e
Alessandro Argenziano <1603alessandro@gmail.com>
2025-12-31 16:43:52 +0100

    feat: implemented breadcrumbs

--- src/components/file-explorer.tsx
+++ src/components/file-explorer.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from "react";
+import { Fragment, useCallback, useMemo, useState } from "react";
 import {
   ResizableHandle,
   ResizablePanel,
@@ -6,10 +6,24 @@ import {
 } from "./ui/resizable";
 import { Hint } from "./hint";
 import { Button } from "./ui/button";
-import { CopyIcon } from "lucide-react";
+import {
+  CheckCheckIcon,
+  ClipboardPasteIcon,
+  CopyCheckIcon,
+  CopyIcon,
+} from "lucide-react";
 import { CodeView } from "./code-view";
 import { convertFilesToTreeItems } from "@/lib/utils";
 import { TreeView } from "./tree-view";
+import {
+  Breadcrumb,
+  BreadcrumbEllipsis,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "./ui/breadcrumb";
+import { toast } from "sonner";
 
 type FileCollection = { [path: string]: string };
 
@@ -18,11 +32,71 @@ function getLanguageFromExtension(filename: string): string {
   return extension || "text";
 }
 
+interface FileBreadcrumbProps {
+  filePath: string;
+}
+
+const FileBreadcrumb = ({ filePath }: FileBreadcrumbProps) => {
+  const pathSegments = filePath.split("/");
+  const maxSegments = 4;
+
+  const renderBreadcrumbItems = () => {
+    if (pathSegments.length <= maxSegments) {
+      return pathSegments.map((segment, index) => {
+        const isLast = index === pathSegments.length - 1;
+
+        return (
+          <Fragment key={index}>
+            <BreadcrumbItem>
+              {isLast ? (
+                <BreadcrumbPage className="font-medium">
+                  {segment}
+                </BreadcrumbPage>
+              ) : (
+                <span className="text-muted-foreground">{segment}</span>
+              )}
+            </BreadcrumbItem>
+            {!isLast && <BreadcrumbSeparator />}
+          </Fragment>
+        );
+      });
+    } else {
+      const firstSegment = pathSegments[0];
+      const lastSegment = pathSegments[pathSegments.length - 1];
+
+      return (
+        <>
+          <BreadcrumbList>
+            <span className="text-muted-foreground">{firstSegment}</span>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbEllipsis />
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage className="font-medium">
+                {lastSegment}
+              </BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </>
+      );
+    }
+  };
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>{renderBreadcrumbItems()}</BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
 interface FileExplorerProps {
   files: FileCollection;
 }
 
 export const FileExplorer = ({ files }: FileExplorerProps) => {
+  const [copied, setCopied] = useState(false);
   const [selectedFile, setSelectedFile] = useState<string | null>(() => {
     const fileKeys = Object.keys(files);
     return fileKeys.length > 0 ? fileKeys[0] : null;
@@ -40,6 +114,23 @@ export const FileExplorer = ({ files }: FileExplorerProps) => {
     },
     [files]
   );
+
+  const handleCopy = useCallback(() => {
+    if (selectedFile) {
+      navigator.clipboard.writeText(files[selectedFile]);
+      setCopied(true);
+      toast("Copied to clipboard.", {
+        description: (
+          <div className="flex items-center gap-2">
+            <p>Paste direct in your editor</p>
+            <ClipboardPasteIcon size={14} />
+          </div>
+        ),
+      });
+
+      setTimeout(() => setCopied(false), 2000);
+    }
+  }, [selectedFile, files]);
 
   return (
     <ResizablePanelGroup orientation="horizontal">
@@ -60,15 +151,16 @@ export const FileExplorer = ({ files }: FileExplorerProps) => {
         {selectedFile && files[selectedFile] ? (
           <div className="h-full w-full flex flex-col">
             <div className="border-b bg-sidebar px-4 py-2 flex justify-between items-center gap-x-2">
+              <FileBreadcrumb filePath={selectedFile} />
               <Hint text="Copy to clipboard" side="bottom">
                 <Button
                   variant="outline"
                   size="icon"
                   className="ml-auto"
-                  onClick={() => {}}
-                  disabled={false}
+                  onClick={handleCopy}
+                  disabled={copied}
                 >
-                  <CopyIcon />
+                  {copied ? <CheckCheckIcon /> : <CopyIcon />}
                 </Button>
               </Hint>
             </div>
